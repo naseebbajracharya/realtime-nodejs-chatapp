5d1bfe7dd776df29bb80049e38bc1c9f41fbd36b
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-02-27 12:35:08 +0545

    changes in UI parts and some functions

--- passport/passport-local.js
+++ passport/passport-local.js
@@ -30,7 +30,7 @@ passport.use('local.signup', new LocalStrategy({
         //else
         const newUser = new User();
         newUser.username = req.body.username;
-        newUser.fullname = req.body.username;
+        newUser.fullname = req.body.fullname;
         newUser.email = req.body.email;
         newUser.password = newUser.encryptPassword(req.body.password);
 
